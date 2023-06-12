e1b383ddec4e9ff8954d544736375c9060807129
Alumno <alumno@sistemas-utnfra.com>
2023-06-12 12:29:40 -0300

    pepe

--- clases.py
+++ clases.py
@@ -19,3 +19,32 @@ class Nave(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.x += self.velocidad_x
+        self.rect.y += self.velocidad_y
+class Asteroide(pygame.sprite.Sprite):
+    def __init__(self ,path_imagen:str , size:tuple , center:tuple,speed):
+        super().__init__()#"""Llamar al  constructor del padre
+        self.image=pygame.image.load(path_imagen).convert_alpha()
+        self.image=pygame.transform.scale(self.image ,size)
+
+        self.rect = self.image.get_rect() #guardar el rect de la imagen
+        self.rect.center=center
+
+        self.velocidad_y=speed
+
+    def update(self):
+        self.rect.y += self.velocidad_y
+
+class laser(pygame.sprite.Sprite):
+    def __init__(self ,path_imagen:str , mid_bottom:tuple):
+        super().__init__()#"""Llamar al  constructor del padre
+        self.image=pygame.image.load(path_imagen).convert_alpha()
+        self.image=pygame.transform.scale(self.image ,size)
+
+        self.rect = self.image.get_rect() #guardar el rect de la imagen
+        self.rect.midbottom=mid_bottom
+
+        self.velocidad_x=0
+        self.velocidad_y=0
+
+    def disparar(self):
+        laser=laser(self.rect.midtop)
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from config import*
 from clases import Nave #[1]
-import pygame ,sys
+from clases import Asteroide
+import pygame ,sys, random
+
+def generar_asteroides(grupo_asteroides , grupo_sprites , cantidad : int):
+    if len(grupo_asteroides) == 0:
+        for i in range(cantidad):
+            posicion=(random.randrange(40,WIDTH-20)),random.randrange(-500,HEIGHT // 2)
+            asteroides=Asteroide("./images/raptor.png", SIZE_ASTEROIDE,posicion,SPEED_ASTEROIDE )
+            grupo_asteroides.add(asteroides)
+            grupo_sprites.add(asteroides)
+
 
 pygame.init()
 relog=pygame.time.Clock()
@@ -8,9 +18,15 @@ relog=pygame.time.Clock()
 screen=pygame.display.set_mode(SIZE)
 pygame.display.set_caption("Nuevo proyecto")
 fondo=pygame.image.load("./images/fondo.jpg").convert()
-#llamar a la funcion constructora [1]
-nave= Nave("./images/nae.png",SIZE_SHIP,(screen.get_width()//2,screen.get_height() - 20 ))
 
+sprites=pygame.sprite.Group()
+asteroides=pygame.sprite.Group()
+lasers=pygame.sprite.Group()
+
+#llamar a la funcion constructora [1]
+nave= Nave("./images/raptor.png",SIZE_SHIP,(screen.get_width()//2,screen.get_height() - 20 ))
+sprites.add(nave)#agregamos la nave al grupo de los sprites 
+generar_asteroides(asteroides,sprites,MAX_ASTEROIDES)
 
 fondo=pygame.transform.scale(fondo,(WIDTH,HEIGHT))
 while True:
@@ -21,17 +37,63 @@ while True:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             nave.rect.center = pygame.mouse.get_pos()
+
         elif event.type == pygame.KEYDOWN:
             if event.key== pygame.K_LEFT:
                 nave.velocidad_x = -SPEED_SHIP
             if event.key== pygame.K_RIGHT:
                 nave.velocidad_x = SPEED_SHIP
 
-    nave.update()
-  
+            if event.key== pygame.K_UP:
+                nave.velocidad_y = -SPEED_SHIP
+            if event.key== pygame.K_DOWN:
+                nave.velocidad_y = SPEED_SHIP
+                
+        elif event.type == pygame.KEYUP:
+            if event.key== pygame.K_UP:
+                nave.velocidad_x = 0
+            if event.key== pygame.K_DOWN:
+                 nave.velocidad_x = 0
 
-    
+
+        pygame.sprite.collide_rect()
+
+    lista=generar_asteroides(asteroides,sprites,MAX_ASTEROIDES)
+    sprites.update()
+
+    if nave.rect.left <=0:
+        nave.rect.left = 0
+    elif nave.rect.right >= WIDTH:
+        nave.rect.right = WIDTH
+
+    if nave.rect.top <=0:
+        nave.rect.top = 0
+    elif nave.rect.bottom >= HEIGHT:
+        nave.rect.bottom = HEIGHT
+
+    for asteroide in asteroides:
+        if asteroide.rect.bottom >= HEIGHT:
+            asteroide.kill()
+
+    pygame.sprite.spritecollide(nave , asteroide , True)
+          
+        # if event.key==pygame.K_UP:
+        #     pass
+        # if event.key==pygame.K_DOWN:
+        #     pass   
+
+ 
     screen.blit(fondo,(0,0))
-    screen.blit(nave.image,nave.rect)
+    sprites.draw(screen)
+    #screen.blit(nave.image,nave.rect)
     pygame.display.flip()
 
+
+#Notaciones
+"""
+
+sonido = pygame.mixed.sound/laser.mp3
+
+
+
+"""
